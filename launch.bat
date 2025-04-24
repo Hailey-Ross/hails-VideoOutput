a311ff95cd9225dad8bab62e8fa64d0c8a9bd52a
Hails <26784904+Hailey-Ross@users.noreply.github.com>
2025-04-23 20:41:30 -0600

    bleepining

--- launch.bat
+++ launch.bat
@@ -3,7 +3,7 @@ REM Use the following for Firefox, the kiosk mode does not perform the same as C
 REM start firefox --kiosk "https://test.hails.cc/cam.html"
 
 REM Use the following if using the video.html file locally
-REM start chrome --app="file:///H:\cam-page\obs-cam.html"
+REM start chrome --app="file:///H:/path/to/video.html"
 
 REM Default way to use this file or Ez-Mode
 start chrome --app="https://test.hails.cc/cam.html"
