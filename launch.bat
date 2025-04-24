f8e9a326c275359b681767a26be0979c25bcc3d5
Hails <26784904+Hailey-Ross@users.noreply.github.com>
2025-04-23 20:08:04 -0600

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -1,4 +1,6 @@
 @echo off
+REM Use the following for Firefox, the kiosk mode does not perform the same as Chrome
 REM start firefox --kiosk "https://test.hails.cc/cam.html"
+REM Use the following if using the video.html file locally
 REM start chrome --app="file:///H:\cam-page\obs-cam.html"
 start chrome --app="https://test.hails.cc/cam.html"
